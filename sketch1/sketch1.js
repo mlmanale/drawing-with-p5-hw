db13dc7194dbc53e2fadf23f885ef17a6fce05e1
mlmanale <97977951+mlmanale@users.noreply.github.com>
2023-01-19 10:30:45 -0600

    setting up

--- sketch1/sketch1.js
+++ sketch1/sketch1.js
@@ -1,16 +1,7 @@
 function setup() {
-    createCanvas(windowWidth, windowHeight);
+    createCanvas(400,400);
 }
 
 function draw() {
-    background(0,255,0);
-    ellipseMode(CORNER); 
-    ellipse(30, 30, 100);
-    square(160, 30, 100);
-    fill('white');
-    outline('black');
-}
-
-function windowResized() {
-    resizeCanvas(windowWidth, windowHeight);
+    background('green')
 }
