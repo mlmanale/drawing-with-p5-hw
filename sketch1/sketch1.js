c25448a288a7f69a7353f76e82bd585bcfac34e7
mlmanale <97977951+mlmanale@users.noreply.github.com>
2023-01-19 10:38:19 -0600

    sketch 1 done

--- sketch1/sketch1.js
+++ sketch1/sketch1.js
@@ -1,7 +1,11 @@
 function setup() {
-    createCanvas(400,400);
+  createCanvas(400, 400);
 }
 
 function draw() {
-    background('green')
+  background(0,255,0);
+  ellipseMode(CORNER);
+  ellipse(30,30,100);
+  square(160, 30, 100);
+  fill('white');
 }
